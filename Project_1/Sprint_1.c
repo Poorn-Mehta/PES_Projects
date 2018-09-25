00e47d5df6877e99fef5614a8d7fb90a97101f5a
Poorn Mehta <Poorn.Mehta@Colorado.EDU>
2018-09-25 13:30:29 -0600

    Adding Sprint 1 Files

---
+++ Project_1/Sprint_1.c
@@ -0,0 +1,38 @@
+/*
+ * Trial.c
+ *
+ *  Created on: Sep 24, 2018
+ *      Author: root
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+#include "Sprint_1.h"
+
+int main(void)
+{
+	char str1[10];
+	printf("Welcome");
+	while(1)
+	{
+		printf("\nEnter Command: ");
+		scanf("%s",str1);
+			if((strcmp(str1, help) == 0) || (strcmp(str1, exit) == 0))
+			{
+				if(strcmp(str1, help) == 0)
+				{
+					printf("You have typed help");
+				}
+				else
+				{
+					printf("Exiting");
+					break;
+				}
+			}
+			else
+			{
+				printf("Invalid Command");
+			}
+	}
+}
---
+++ Project_1/Sprint_1.h
@@ -0,0 +1,16 @@
+/*
+ * Sprint_1.h
+ *
+ *  Created on: Sep 24, 2018
+ *      Author: root
+ */
+
+#ifndef SPRINT_1_H_
+#define SPRINT_1_H_
+
+#define help	"help"
+#define exit	"exit"
+
+
+
+#endif /* SPRINT_1_H_ */
