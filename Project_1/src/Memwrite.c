a521e65cc845d444cb7908945af2029849611173
Poorn-Mehta <Poorn.Mehta@Colorado.EDU>
2018-10-04 00:32:40 -0600

    Update Testing

--- Project_1/src/Memwrite.c
+++ Project_1/src/Memwrite.c
@@ -17,15 +17,16 @@ void memwrite(void)
 			}
 		}
 		mem_ptr = mem_original;
-		if(relative_address)
+/*		if(relative_address)
 		{
 			value1 = (value1 * 4) + (uint32_t)mem_ptr;
 		}
-		mem_ptr2 = (uint32_t *) value1;
+		mem_ptr2 = (uint32_t *) value1;*/
 		if(print == 0)
 		{
 			printf("\nThe hex data at the specified location BEFORE writing %x is %x\n", mem_ptr2, *mem_ptr2);
 		}
+		mem_ptr2 = mem_ptr;
 		*mem_ptr2 = value2;
 		if(print == 0)
 		{
