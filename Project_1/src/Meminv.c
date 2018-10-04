8135b151f218a6dacbb8f5679d150e07a914a223
Poorn-Mehta <Poorn.Mehta@Colorado.EDU>
2018-10-04 12:14:39 -0600

    Updated meminv.c

--- Project_1/src/Meminv.c
+++ Project_1/src/Meminv.c
@@ -11,7 +11,7 @@ void meminv(void)
 		mem_ptr = mem_original;
 		if(relative_address)
 		{
-			value1 = (value1 * 4) + (uint32_t)mem_ptr;
+			value1 = (value1 * 4) + (uint64_t)mem_ptr;
 		}
 		mem_ptr2 = (uint32_t *) value1;
 		if(mem_ptr2)
@@ -30,7 +30,7 @@ void meminv(void)
 					printf("\nThe allocated addresses are as below:\n");
 					for(mem_i = 0; mem_i < mem_max; mem_i ++, mem_ptr ++)
 					{
-						printf("\nRelative address: %d \t\t Actual address: %x \t\t Existing hex data at this location in Hex: %x\n", mem_i, mem_ptr, *mem_ptr);
+						printf("\nRelative address: %d \t\t Actual address: %p \t\t Existing hex data at this location in Hex: %x\n", mem_i, mem_ptr, *mem_ptr);
 					}
 				}
 				if(print == 0)
@@ -50,7 +50,7 @@ void meminv(void)
 					printf("\nThe allocated addresses are as below:\n");
 					for(mem_i = 0; mem_i < mem_max; mem_i ++, mem_ptr ++)
 					{
-						printf("\nRelative address: %d \t\t Actual address: %x \t\t Existing hex data at this location in Hex: %x\n", mem_i, mem_ptr, *mem_ptr);
+						printf("\nRelative address: %d \t\t Actual address: %p \t\t Existing hex data at this location in Hex: %x\n", mem_i, mem_ptr, *mem_ptr);
 					}
 				}
 				mem_ptr = mem_original;
